dc2d10099f9328c807125e7c2cc9f2acbe80d54d
Develon2015 <302615249@qq.com>
2020-10-14 10:13:03 +0800

    get IP

--- get-remote-ip.js
+++ get-remote-ip.js
@@ -1,5 +1,5 @@
 function getRemoteIP(request) {
-    return request.header('cf-connecting-ip') || '未知IP';
+    return request.header('cf-connecting-ip') || request.ip || '未知IP';
 }
 
 module.exports = getRemoteIP;
